773c2cc54bc2935de96636b929817b6e4218953c
Will <will@mockdesign.com>
2017-10-15 01:04:33 -0500

    Adding work for after response

---
+++ Will/drop.js
@@ -0,0 +1,44 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
+$('.dropbtn').on("click", dropToggle);
+
+function dropToggle() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+//watch fot the results to be clicked
+$('.result').click(function setResult(){
+  var url = $(this).children('.url').text();
+  var word = $(this).closest('.dropdown').ignore('div').text();
+  $(this).closest('.dropdown').replaceWith('<a href=' + url + '>' + word + '</a>');
+});
+
+//watching for custom entry
+$('.custom').click(function enterCustomURL(){
+  var url = $(this).children('.url').text();
+  var word = $(this).closest('.dropdown').ignore('div').text();
+  var pop = prompt("Enter a URL", "https://www.Google.com");
+  $(this).closest('.dropdown').replaceWith('<a href = ' + pop + '>' + word + '</a>');
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+$( document ).ready(function() {
+  $.fn.ignore = function(sel){
+    return this.clone().find(sel||">*").remove().end();
+  };
+});
--- Will/entitySearch.js
+++ Will/entitySearch.js
@@ -1,20 +1,26 @@
 //getting the text from the text area
-$("#submitBtn").on("click", textGrab);
+$("#submitBtn").on("click", textGrab2);
 
 function textGrab(){
 
-  var text = {inputText: $("#textarea").val()};
+  var text = $("#textarea").val();
 
   console.log(text);
 
-  var url = "localhost:15000";
-  $.ajax({
-    type: 'POST',
-    url: url,
-    async: false,
-    data: text,
-    success: function(resp){
-        console.log(resp);
-    }
+  var reg = new RegExp("Google", 'gi');
+
+  var txt = $("#textarea").val().replace(reg, function(str) {
+      return "<span class='highlight'>" + str + "</span>"
   });
+};
+
+function textGrab2(){
+
+  var find = 'Google';
+  var re = new RegExp(find, 'g');
+
+  str = $("#textarea").val().replace(re, 'TEST');
+
+  console.log(str);
+
 };
